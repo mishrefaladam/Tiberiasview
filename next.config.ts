6eb73e8b0605779b2372f10942b162dc46d83272
mischrifalazm <wi24b137@technikum-wien.at>
2026-09-03 03:17:32 +0200

    Configure custom domain

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 import type { NextConfig } from "next";
 
 const isGithubPages = process.env.GITHUB_PAGES === "true";
-const githubPagesBasePath = "/Tiberiasview";
 
 const nextConfig: NextConfig = {
   output: isGithubPages ? "export" : undefined,
-  basePath: isGithubPages ? githubPagesBasePath : undefined,
-  assetPrefix: isGithubPages ? `${githubPagesBasePath}/` : undefined,
   trailingSlash: isGithubPages,
   images: {
     formats: ["image/avif", "image/webp"],
